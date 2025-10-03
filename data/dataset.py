1439203ff96dabf198e9676a3b14bf0956a1abeb
mxl <mxlscut153@163.com>
2025-10-03 10:57:25 +0800

    add dataset.py

---
+++ data/dataset.py
@@ -0,0 +1,92 @@
+from datasets import load_dataset, DownloadConfig
+from transformers import AutoTokenizer, AutoProcessor
+from transformers import DataCollatorForSeq2Seq
+from torch.utils.data import Dataset
+from PIL import Image
+import torch
+import random
+import io
+import os
+
+os.environ["HTTP_PROXY"] = "http://127.0.0.1:7890"
+os.environ["HTTPS_PROXY"] = "http://127.0.0.1:7890"
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+# Tokenizer
+qwen_path = os.path.join(base_path, "..", "Qwen3-0.6B")
+clip_path = os.path.join(base_path, "..", "clip-vit-base-patch16")
+
+
+dataset_Multi_CN = str()
+
+# 训练中文闲聊能力并不需要图片，所以不需要CLIP的Processor
+class LoRADataset_Multi(dataset):
+    def __init__(
+        self,
+        qwen_path,
+        config=None,
+        split_type='train',
+        val_split_ratio=0.05,
+    ):
+        super().__init__()
+        dataset_name = "BelleGroup/multiturn_chat_0.8M"
+        print(f"加载 {split_type} 数据集 ({dataset_name})")
+
+        raw_dataset = load_dataset(dataset_name, split="train", cache_dir=base_path)
+        
+        split_dataset = raw_dataset.train_test_split(test_size=val_split_ratio, seed=520)
+        # 这里有分train和test的数据集
+        if split_type == "train":
+            self.dataset = split_dataset["train"]
+            print(f"训练集大小: {len(self.dataset)}")
+        else:
+            self.dataset = split_dataset["test"]
+            print(f"验证集大小: {len(self.dataset)}")
+            
+        # 加载Qwen和tokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(qwen_path)
+        self.pad_token_id = self.tokenizer.pad_token_id
+        
+        
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, idx):
+        item = self.dataset[idx]
+        
+        # 数据集有instruction和output
+        full_text = item["instruction"] + item["output"]
+        # tokenization
+        full_input_ids = self.tokenizer(full_text)["input_ids"]
+        
+        prompt_text = item["instruction"]
+        
+        # prompt长度,后面切割用
+        prompt_len = len(self.tokenizer(prompt_text)["input_ids"])
+        
+        # labels是output的内容,前面用pad补齐长度
+        labels = [self.pad_token_id] * prompt_len + full_input_ids[prompt_len:]
+        
+        #shift
+        input_ids = full_input_ids[:-1]
+        labels = labels[1:]
+        
+        
+        return{
+            "input_ids": input_ids,
+            "labels": labels,
+        }
+        
+
+class MTalkDataCollator(DataCollatorForSeq2Seq):
+    
+    def __call__(self, features, return_tensors=None):
+        
+        batch = super().__call__(features, return_tensors)
+        
+        return batch
+        
+
+if __name__ == "__main__":
+    dataset = LoRADataset_Multi(qwen_path)
+    print(random.choice(dataset))
