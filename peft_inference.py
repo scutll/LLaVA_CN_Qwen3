50a1e614580e6beb661ecf60210580444de87e17
scutll <mxlscut153@163.com>
2025-10-04 23:36:41 +0800

    apply three choice for token generation

--- peft_inference.py
+++ peft_inference.py
@@ -5,13 +5,13 @@ from transformers import AutoTokenizer, AutoProcessor
 from safetensors.torch import load_file as load_safetensors
 from peft import get_peft_model, LoraConfig, TaskType
 from path_config import qwen_config_path, clip16_config_path
+from model import VLM, VLMConfig
+import argparse
 
-from model import VLM_without_CLIP, VLMConfig
-
-def generate_response():
+def generate_response(mode, k):
     
     base_path = os.path.dirname(os.path.abspath(__file__))
-    checkpoint_path = os.path.join(base_path, "stage2_loss1p8", "lora_best_model") 
+    checkpoint_path = os.path.join(base_path, "stage2_loss1p8", "checkpoint-6400") 
     qwen_path = os.path.join(base_path, qwen_config_path)
     clip_path = os.path.join(base_path, clip16_config_path) 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -21,7 +21,7 @@ def generate_response():
     config = VLMConfig(qwen_path=qwen_path, clip_path=clip_path, dtype=torch.bfloat16)
     
     print("加载基础模型结构...")
-    model = VLM_without_CLIP(config)
+    model = VLM(config)
 
     # 加载LoRA微调好的模型
     print("配置并应用LoRA结构...")
@@ -63,6 +63,8 @@ def generate_response():
     # 多轮对话的记忆
     conversation = [{"role": "system", "content": "You are a helpful assistant."}]
     pixel_values = None
+    
+    Mode = mode
     
     while True:
         try:
@@ -107,6 +109,12 @@ def generate_response():
             max_new_tokens = 256  
             eos_token_id = tokenizer.eos_token_id
             
+            # 模式选择 
+            # Greedy: 选择概率最大的token
+            # Top-k Sampling: 在top-k 候选集合中随机选择
+            # Hybrid-Smart 在top-k中随机选择，当<eos>概率最大时直接选择eos
+            print(f"token生成模式: {Mode}")    
+            
             # 生成过程依然是每次一个token
             with torch.no_grad():
                 for _ in range(max_new_tokens):
@@ -117,19 +125,51 @@ def generate_response():
                                         labels=None)
                         
                     next_token_logits = outputs.logits[:, -1, :]
-                    # next_token_id = torch.argmax(next_token_logits, dim=-1)
-                    probs = torch.softmax(next_token_logits, dim=-1)
-                    topk_probs, topk_indices = torch.topk(probs, k=20, dim=-1)
                     
-                    next_token_id = topk_indices[0, torch.multinomial(topk_probs[0], num_samples=1)]
-                    next_token_id = next_token_id.unsqueeze(0)
+                    # Greedy
+                    if Mode == "Greedy":
+                        next_token_id = torch.argmax(next_token_logits, dim=-1)
+                        if next_token_id.item() == eos_token_id:
+                            print("(检测到结束符)")
+                            break
+                        else: 
+                            input_ids = torch.cat([input_ids, next_token_id.unsqueeze(0)], dim=1)
+                            continue
                     
-                    if next_token_id.item() == eos_token_id:
-                        print("(检测到结束符)")
+                    # Top-k
+                    if Mode == "TopkS":
+                        # 使用top20来随机选择token
+                        probs = torch.softmax(next_token_logits, dim=-1)
+                        topk_probs, topk_indices = torch.topk(probs, k=k, dim=-1)
+                        
+                        next_token_id = topk_indices[0, torch.multinomial(topk_probs[0], num_samples=1)]
+                        next_token_id = next_token_id.unsqueeze(0)
+                        
+                        if next_token_id.item() == eos_token_id:
+                            print("(检测到结束符)")
+                            break
+                        input_ids = torch.cat([input_ids, next_token_id], dim=1)
+                        continue
+                    
+                    # Hybird
+                    if Mode == "Hybrid":                   
+                        next_token_id = torch.argmax(next_token_logits, dim=-1)
+                        if next_token_id == eos_token_id:
+                            print("检测到结束符优先")
+                            break
+                        probs = torch.softmax(next_token_logits, dim=-1)
+                        topk_probs, topk_indices = torch.topk(probs, k=k, dim=-1)
+                        
+                        next_token_id = topk_indices[0, torch.multinomial(topk_probs[0], num_samples=1)]
+                        next_token_id = next_token_id.unsqueeze(0)
+                        
+                        input_ids = torch.cat([input_ids, next_token_id], dim=1)
+                        
+                        continue
+                    
+                    else:
+                        print("Error: Unknown token generating Mode")
                         break
-                    input_ids = torch.cat([input_ids, next_token_id], dim=1)
-                    
-                    # input_ids = torch.cat([input_ids, next_token_id.unsqueeze(0)], dim=1)
 
             response_ids = input_ids[:, initial_prompt_len:]
             response_text = tokenizer.decode(response_ids[0], skip_special_tokens=True)
@@ -148,4 +188,11 @@ def generate_response():
             print(f"发生错误: {e}")
 
 if __name__ == "__main__":
-    generate_response()
+    parser = argparse.ArgumentParser(description="")
+    
+    parser.add_argument("--Mode", type=str, choices=["Greedy", "TopkS", "Hybrid"], default="Hybrid", help="token generater mode -Greedy -Top-k_Sampling -Hybrid(default)")
+    parser.add_argument("--k", type=int, default=20, help="top-k randomly selection")
+    
+    args = parser.parse_args()
+
+    generate_response(mode=args.Mode, k=args.k)
